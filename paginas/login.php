792c0a854d3bbe898d703e0cf040451d2e23cc30
jeyb747 <samueldiaz.g2008@gmail.com>
2026-08-12 20:31:11 -0500

    Evita caché desactualizada de estilos del login

--- paginas/login.php
+++ paginas/login.php
@@ -19,7 +19,7 @@ session_start();
 
   <!-- CSS -->
   <link rel="stylesheet" href="../css/inicio.css">
-  <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="../css/login.css?v=20260812-2">
   <link rel="stylesheet" href="../css/alerts.css">
 
 </head>
